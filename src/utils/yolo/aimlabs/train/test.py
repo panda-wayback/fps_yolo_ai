70cc640ec21870dd9c190e50ec786931a1911fbf
Panda <wenchenglong716@gmail.com>
2025-10-01 12:01:54 +0800

    Please enter the commit message for your changes. Lines starting with '#' will be ignored, and an empty message aborts the commit.
    On branch panda/update
    Your branch is up to date with 'origin/panda/update'.
    
    Changes to be committed:
    	deleted:    src/utils/move_mouse/move_mouse.py
    	deleted:    src/utils/yolo/aimlabs/train/README.md
    	deleted:    src/utils/yolo/aimlabs/train/main.py
    	deleted:    src/utils/yolo/aimlabs/train/requirements.txt
    	deleted:    src/utils/yolo/aimlabs/train/test.py
    	deleted:    src/utils/yolo/aimlabs/train/test_mps.py
    	deleted:    src/utils/yolo/aimlabs/train/train_fast.py
    	deleted:    src/utils/yolo/aimlabs/train/train_simple.py
    	deleted:    src/utils/yolo/aimlabs/train/train_ultra_fast.py

--- src/utils/yolo/aimlabs/train/test.py
+++
@@ -1,6 +0,0 @@
-import torch
-print(torch.__version__)
-torch.backends.mps.is_available()  # True 表示可以用 MPS 加速
-print(torch.backends.mps.is_available())
-device = torch.device("mps" if torch.backends.mps.is_available() else "cpu")
-print(device)
